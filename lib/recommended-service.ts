afd5ebb179fd70640ae895fcbfcbbac58cf0252a
Renegildo <renegildo@proton.me>
2024-04-12 09:34:19 -0300

    remove delay getting recommendations

--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -2,8 +2,6 @@ import { db } from "./db";
 import { getSelf } from "./auth-service";
 
 export const getRecommended = async () => {
-	await new Promise(resolve => setTimeout(resolve, 5000));
-
 	const users = await db.user.findMany({
 		orderBy: {
 			createdAt: 'desc',
