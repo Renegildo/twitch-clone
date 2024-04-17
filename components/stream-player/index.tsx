167b2587a129361ee1cd622de79b7c812d4d99b5
Renegildo <renegildo@proton.me>
2024-04-17 15:27:18 -0300

    add about card

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -3,7 +3,7 @@
 import { cn } from "@/lib/utils";
 import { useChatSidebar } from "@/store/use-chat-sidebar";
 import { useViewerToken } from "@/hooks/use-viewer-token";
-import { Stream, User } from "@prisma/client";
+import { Follow, Stream, User } from "@prisma/client";
 
 import { LiveKitRoom } from '@livekit/components-react'
 import Video, { VideoSkeleton } from "./video";
@@ -11,9 +11,15 @@ import Chat, { ChatSkeleton } from "./chat";
 import ChatToggle from "./chat-toggle";
 import Header, { HeaderSkeleton } from "./header";
 import InfoCard from "./info-card";
+import AboutCard from "./about-card";
 
 interface StreamPlayerProps {
-	user: User & { stream: Stream | null };
+	user: User & {
+		stream: Stream | null,
+		_count: {
+			followedBy: number
+		},
+	};
 	stream: Stream;
 	isFollowing: boolean;
 };
@@ -70,6 +76,13 @@ const StreamPlayer = ({
 						viewerIdentity={identity}
 						name={stream.name}
 						thumbnailUrl={stream.thumbnailUrl}
+					/>
+					<AboutCard
+						hostName={user.username}
+						hostIdentity={user.id}
+						viewerIdentity={identity}
+						bio={user.bio}
+						followedByCount={user._count.followedBy}
 					/>
 				</div>
 				<div className={cn(
--- lib/user-service.ts
+++ lib/user-service.ts
@@ -3,7 +3,14 @@ import { db } from "./db"
 export const getUserByUsername = async (username: string) => {
 	const user = await db.user.findUnique({
 		where: { username },
-		include: { stream: true }
+		include: {
+			stream: true,
+			_count: {
+				select: {
+					followedBy: true,
+				},
+			},
+		},
 	});
 
 	return user;
